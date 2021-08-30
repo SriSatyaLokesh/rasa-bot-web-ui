b1de8266a88a7ec3db61cd79772e72daaa0bbffb
SriSatyaLokesh <srisatyalokesh2.12@gmail.com>
2021-08-30 21:07:26 +0530

    new ngrok

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "";
-const rasa_server_url = "https://c49b-43-228-95-6.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "https://0dae-43-228-95-6.ngrok.iowebhooks/rest/webhook";
 const sender_id = uuidv4();
