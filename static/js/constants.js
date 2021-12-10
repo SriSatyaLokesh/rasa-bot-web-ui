c330e8cd9d5cb730d1800a17a14349b130f505f1
Satya <43106076+SriSatyaLokesh@users.noreply.github.com>
2021-12-10 18:01:39 +0530

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "";
-const rasa_server_url = "http://a0e7-103-90-158-182.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "https://a0e7-103-90-158-182.ngrok.io/webhooks/rest/webhook";
 const sender_id = uuidv4();
